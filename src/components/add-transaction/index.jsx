dc8f58ffe49a08927436c8e7e4f409422f74df5d
Abdul <abdulsalawu12@gmail.com>
2024-04-23 12:25:29 -0600

    completed functionality - expense report now works for income and expenses

--- src/components/add-transaction/index.jsx
+++ src/components/add-transaction/index.jsx
@@ -12,11 +12,28 @@ import {
   Radio,
   RadioGroup,
 } from "@chakra-ui/react";
+import { useContext } from "react";
+import { GlobalContext } from "../../context";
 
 export default function TransactionForm({ onClose, isOpen }) {
+  const { formData, setFormData, value, setValue, handleFormSubmit } =
+    useContext(GlobalContext);
+
+  function handleFormChange(event) {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    handleFormSubmit(formData);
+  }
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <form>
+      <form onSubmit={handleSubmit}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Add New Transaction</ModalHeader>
@@ -28,6 +45,7 @@ export default function TransactionForm({ onClose, isOpen }) {
                 placeholder="Enter Transaction description"
                 name="description"
                 type="text"
+                onChange={handleFormChange}
               />
             </FormControl>
             <FormControl>
@@ -36,13 +54,26 @@ export default function TransactionForm({ onClose, isOpen }) {
                 placeholder="Enter Transaction amount"
                 name="amount"
                 type="number"
+                onChange={handleFormChange}
               />
             </FormControl>
-            <RadioGroup mt={"5"}>
-              <Radio value="income" colorScheme="blue" name="type">
+            <RadioGroup mt={"5"} value={value} onChange={setValue}>
+              <Radio
+                checked={formData.type === "income"}
+                value="income"
+                colorScheme="blue"
+                name="type"
+                onChange={handleFormChange}
+              >
                 Income
               </Radio>
-              <Radio value="expense" colorScheme="red" name="type">
+              <Radio
+                checked={formData.type === "expense"}
+                value="expense"
+                colorScheme="red"
+                name="type"
+                onChange={handleFormChange}
+              >
                 Expense
               </Radio>
             </RadioGroup>
@@ -51,7 +82,9 @@ export default function TransactionForm({ onClose, isOpen }) {
             <Button mr={"4"} onClick={onClose}>
               Cancel
             </Button>
-            <Button>Add</Button>
+            <Button onClick={onClose} type="submit">
+              Add
+            </Button>
           </ModalFooter>
         </ModalContent>
       </form>
--- src/context/index.jsx
+++ src/context/index.jsx
@@ -1,7 +1,45 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const GlobalContext = createContext(null);
 
-export default function GlobalState({children}){
-    return <GlobalContext.Provider>{children}</GlobalContext.Provider>
+export default function GlobalState({ children }) {
+  const [formData, setFormData] = useState({
+    type: "expense",
+    amount: 0,
+    description: "",
+  });
+
+  const [value, setValue] = useState("expense");
+  const [totalExpense, setTotalExpense] = useState(0);
+  const [totalincome, setTotalIncome] = useState(0);
+  const [allTransactions, setAllTransactions] = useState([]);
+
+  function handleFormSubmit(currentFormData) {
+    if (!currentFormData.description || !currentFormData.amount) return;
+    setAllTransactions([...allTransactions], {
+      ...currentFormData,
+      id: Date.now(),
+    });
+  }
+
+  console.log(allTransactions);
+  return (
+    <GlobalContext.Provider
+      value={{
+        formData,
+        setFormData,
+        totalExpense,
+        setTotalExpense,
+        totalincome,
+        setTotalIncome,
+        value,
+        setValue,
+        allTransactions,
+        setAllTransactions,
+        handleFormSubmit,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
 }
